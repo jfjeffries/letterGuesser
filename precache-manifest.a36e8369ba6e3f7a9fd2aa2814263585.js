365f8b4015b16f5c46ec0f2e78eba8d1718eca7b
jfjeffries <jon.f.jeffries@gmail.com>
2020-09-29 20:12:16 -0400

    Updates

---
+++ precache-manifest.a36e8369ba6e3f7a9fd2aa2814263585.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e25f0d672f6d70c1a1434410d4b4c1",
+    "url": "/letterGuesser/index.html"
+  },
+  {
+    "revision": "6ca3b533c519293110cc",
+    "url": "/letterGuesser/static/css/main.c79d2ab5.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/letterGuesser/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "6ca3b533c519293110cc",
+    "url": "/letterGuesser/static/js/main.176c76f9.chunk.js"
+  },
+  {
+    "revision": "55c589b53a813538cd5f",
+    "url": "/letterGuesser/static/js/runtime~main.36d10640.js"
+  },
+  {
+    "revision": "ae6a12c3f95175fa4ffdfaac3357dc17",
+    "url": "/letterGuesser/static/media/armyman4.ae6a12c3.png"
+  },
+  {
+    "revision": "6433c826f7e0da1884605b1eb8461d9f",
+    "url": "/letterGuesser/static/media/explosion1.6433c826.svg"
+  },
+  {
+    "revision": "f530da63d1918a5723773852f45f4813",
+    "url": "/letterGuesser/static/media/explosion2.f530da63.svg"
+  },
+  {
+    "revision": "1808275678c165ef92f58cd092bd34a1",
+    "url": "/letterGuesser/static/media/explosion3.18082756.svg"
+  },
+  {
+    "revision": "c5e8cdd1da16ba70e499da46416fe868",
+    "url": "/letterGuesser/static/media/explosion4.c5e8cdd1.svg"
+  }
+]);
